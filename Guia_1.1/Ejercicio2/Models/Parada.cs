64c98f60983e1cdcabf162cd2285012b103e4b47
alevitt <alejandrovitt@hotmail.com>
2025-08-12 12:23:34 -0300

    comienzo del ejercicio 2, planteo de clases

---
+++ Guia_1.1/Ejercicio2/Models/Parada.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    internal class Parada
+    {
+        public int CantPasajerosDescienden {  get; set; }
+        public int CantPasajerosAscienden { get; set; }
+
+        public double TiempoDemora { get; set; }
+
+    }
+}
---
+++ Guia_1.1/Ejercicio2/Models/Viaje.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    internal class Viaje
+    {
+        public int Transportados { get; private set; }
+        public int Asientos { get; private set; }
+        public int HDemora { get; private set; }
+
+        public int MDemora { get; private set;}
+
+        public int CantParadas { get; private set; }
+
+        public int HDuracionViaje { get; private set; } // supongo que por el uml debo usar estas variables como hora de inicio en el sonctructor
+
+        public int MDuracionViaje { get; private set; }
+
+
+        public Viaje(int hInicio, int mInicio , int asientos)
+        {
+            this.HDuracionViaje = hInicio;
+            this.MDuracionViaje = mInicio;
+
+            this.Asientos = asientos;
+
+        }
+
+        public void RealizarParada(int hLlegadaParada,int mLlegadaParada, int hSalidaParada, int mSalidaParada, int asciende, int descienden)
+        {
+
+        }
+        public void FinalizarViaje(int hFinal, int mFinal)
+        {
+
+        }
+    }
+}
